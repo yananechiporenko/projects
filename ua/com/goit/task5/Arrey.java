9c2f9558616ae4f4b05ca3aef8587e8edb4822e9
y.nechiporenko <sokolyana@gmail.com>
2015-12-29 13:56:53 +0200

    add class Arrey

---
+++ ua/com/goit/task5/Arrey.java
@@ -0,0 +1,12 @@
+package ua.com.goit.task5;
+
+/**
+ * Created by y.nechiporenko on 29.12.2015.
+ */
+public class Arrey {
+
+    public static void main (String[] args){
+        int [] myArrey = {3, 4, 6, 2, 1, 8};
+
+    }
+}
