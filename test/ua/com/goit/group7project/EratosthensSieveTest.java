4c32984f3090c0b884e409156cbecdb5b66ba7fe
y.nechiporenko <sokolyana@gmail.com>
2016-02-02 08:35:40 +0200

    add 2-d unit-test

--- test/ua/com/goit/group7project/EratosthensSieveTest.java
+++ test/ua/com/goit/group7project/EratosthensSieveTest.java
@@ -7,7 +7,6 @@ import ua.com.goit.group7project.EratosthensSieve;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.Assert.*;
 
 public class EratosthensSieveTest {
 
@@ -19,6 +18,27 @@ public class EratosthensSieveTest {
         primeNumbers.add(2);
         primeNumbers.add(3);
         primeNumbers.add(5);
+        final EratosthensSieve eratosthensSieve = new EratosthensSieve(n);
+        eratosthensSieve.findPrimeNumbers();
+        final List<Integer> result = eratosthensSieve.getPrimeNumbers();
+        Assert.assertEquals(primeNumbers, result);
+
+    }
+
+    @Test (timeout = 3000)
+    public void testGetPrimeNumbers1() throws Exception {
+        final int n = 30;
+        final List<Integer> primeNumbers = new ArrayList<Integer>();
+        primeNumbers.add(2);
+        primeNumbers.add(3);
+        primeNumbers.add(5);
+        primeNumbers.add(7);
+        primeNumbers.add(11);
+        primeNumbers.add(13);
+        primeNumbers.add(17);
+        primeNumbers.add(19);
+        primeNumbers.add(23);
+        primeNumbers.add(29);
         final EratosthensSieve eratosthensSieve = new EratosthensSieve(n);
         eratosthensSieve.findPrimeNumbers();
         final List<Integer> result = eratosthensSieve.getPrimeNumbers();
